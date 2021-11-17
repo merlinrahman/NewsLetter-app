af794b51c96b1e07ed0e6a91c742eff855c7ff4a
merlinrahman <77335209+merlinrahman@users.noreply.github.com>
2021-11-17 07:22:53 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,76 +1,48 @@
-const express = require("express")
+const express = require("express");
+const https = require("https");
 const bodyParser = require("body-parser")
-const request = require("request")
-const https = require("https")
 
-const app = express()
-
-//we use static method for our server to use static files using the public folder
-app.use(express.static("public"))
-
-
-//we use bodyParser to get user input details from a user form
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 
+//the get request makes request to our server
+app.get("/",(req,res)=>{
 
-
-//creating a get route to get request from our server
-app.get("/", (req,res) =>{
-    res.sendFile(__dirname + "/signup.html")
-})
-
-
-//creating a post route to get response to our server
-app.post("/",(req, res) =>{
-    const firstName = req.body.fName
-    const lastName =req.body.lName
-    const email = req.body.email
+   res.sendFile(__dirname + "/index.html")
     
-
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName
-                }
-            }
-        ]
-    }
-    const jsonData = JSON.stringify(data)
-
-    const url = "https://us20.api.mailchimp.com/3.0/lists/30c7e5a8c3"
-    const options = {
-        method: "POST",
-        auth: "merlin:86805f65221a4b132070c10401463184-us20"
-    }
-    const request = https.request(url,options,(response) =>{
-
-        //checking if there is an error in our code and send the result to a success
-        //or failure page
-        if(response.statusCode == 200){
-            res.sendFile(__dirname + "/success.html")
-        }else{
-            res.sendFile(__dirname + "/failure.html")
-        }
-        response.on("data", (data) => {
-            console.log(JSON.parse(data))
-        })
-    })
-    request.write(jsonData);
-    request.end();
-
 })
 
+//making a post route for our app
+app.post("/", (req, res) =>{
+
+     //creating variables to change some parameters in the endpoint(URL)
+ const query = req.body.cityName
+ const apiKey = "7c28284fde573608fb945e8b71af1fc6"
+ const Unit = "metric"
+
+ // making request to an external api server
+ const url ="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ Unit;
+
+ //get request for https
+ https.get(url,(response)=>{
+     console.log(response)
 
 
+     //fetching our data from the response and change it into a java script object api using the https.get method
+     response.on("data",(data)=>{
+         const weatherData =JSON.parse(data)
+         const temp = weatherData.main.temp
+         const weatherDescription = weatherData.weather[0].description
+         const icon =weatherData.weather[0].icon
+         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+         res.write("<p>The weather is currently " + weatherDescription + "</p>")
+         res.write("<h1>The temperature in "+ query +" is:" + temp + " degrees Celcius.</h1>")
+         res.write("<img src=" + imageURL + ">")
+         res.send()
+     })
+ })
 
-
-//post route for the button to redirect to our home page to try again
-app.post("/failure", (req,res) =>{
-    res.redirect("/")
+    
 })
 
 
@@ -82,13 +54,9 @@ app.post("/failure", (req,res) =>{
 
 
 
-app.listen(process.env.PORT || 3000,() =>{
+
+// our app listen to this port 3000
+app.listen(3000,()=>{
+
     console.log("server is running on port 3000")
 })
-
-
-//API KEY
-//86805f65221a4b132070c10401463184-us20
-
-//AUDIENCE ID
-//30c7e5a8c3
